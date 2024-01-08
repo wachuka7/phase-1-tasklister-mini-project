7ad17372dc10d3ead6c0d3a26dc9d391823e434e
wachuka7 <rachaelwachuka7@gmail.com>
2024-01-09 00:09:33 +0300

    Add Tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const createForm = document.querySelector('#create-task-form');
+const taskList = document.querySelector('#tasks');
+
+createForm.addEventListener('submit', function (event){
+  event.preventDefault();
+
+  const description=document.querySelector('#new-task-description').value;
+  const priority=document.querySelector('#priority').value;
+  const user=document.querySelector('#theUser').value;
+  const taskDuration =document.querySelector('#taskTime').value;
+  const dueDate =document.querySelector('#dueDate').value;
+ 
+  const taskTable =document. querySelector('tr');
+  taskTable.innerHTML=`
+    <td>${description}</td>
+    <td>${priority}</td>
+    <td>${user}</td>
+    <td>${taskDuration}</td>
+    <td>${dueDate}</td>
+    <td><button="deleteTask">Delete</button></td>
+  `;
+  taskList.appendChild(taskTable);
+  document.querySelector('#tasks');
+  // createForm.reset();
+})
 });
+
+//declare a function for deleting a task
+function deleteATask(button){
+  const tasksTable=button.parentNode.parentNode;
+  tasksTable.remove();
+}
